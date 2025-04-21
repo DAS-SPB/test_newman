8cd9c16852ff37dcafa4fe28bcd7baa53f115dc7
AntonioMont <garsiamarkes111@gmail.com>
2025-04-21 17:12:10 +0300

    app

---
+++ main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+from endpoints import create_user, get_user
+
+PREFIX = "/simple_app"
+
+simple_app = FastAPI()
+
+simple_app.include_router(router=create_user.router, prefix=PREFIX)
+simple_app.include_router(router=get_user.router, prefix=PREFIX)
+
+
+@simple_app.get("/health")
+def healthcheck():
+    return {"status": "OK"}
