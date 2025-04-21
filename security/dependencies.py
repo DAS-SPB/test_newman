aa61248057e143ac1eda116f1e58045f96372a80
AntonioMont <garsiamarkes111@gmail.com>
2025-04-21 18:25:09 +0300

    field name fix

--- security/dependencies.py
+++ security/dependencies.py
@@ -8,13 +8,13 @@ def signature_verification(request: Request):
     if not x_api_key:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
-            detail=ErrorResponse(message="Missing x-api-key header", code=101).dict()
+            detail=ErrorResponse(message="Missing x-api-key header", error_code=101).dict()
         )
 
     if x_api_key != settings.x_api_key:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
-            detail=ErrorResponse(message="Invalid x-api-key header", code=101).dict()
+            detail=ErrorResponse(message="Invalid x-api-key header", error_code=101).dict()
         )
 
     return True
