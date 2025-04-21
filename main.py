52887f7144d6efc65613e506b6994f6070eb247b
AntonioMont <garsiamarkes111@gmail.com>
2025-04-21 19:00:48 +0300

    validation handler added

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, status
+from fastapi.exceptions import RequestValidationError
+from fastapi.responses import JSONResponse
 from endpoints import create_user, get_user
+from models.error_response import ErrorResponse
 
 PREFIX = "/simple_app"
 
 simple_app = FastAPI()
+
+
+@simple_app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request: Request, exc: RequestValidationError):
+    return JSONResponse(
+        status_code=status.HTTP_400_BAD_REQUEST,
+        content=ErrorResponse(
+            message="Invalid request",
+            error_code=102
+        ).model_dump()
+    )
+
 
 simple_app.include_router(router=create_user.router, prefix=PREFIX)
 simple_app.include_router(router=get_user.router, prefix=PREFIX)
