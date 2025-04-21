f950e3737d8053c0a47910a68240048736ffb8bd
AntonioMont <garsiamarkes111@gmail.com>
2025-04-21 19:00:27 +0300

    status code update

--- endpoints/create_user.py
+++ endpoints/create_user.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, status
 import uuid
 from security.dependencies import signature_verification
 from models.user import UserInfo, CreatedUser
@@ -7,7 +7,7 @@ from storage.data import db
 router = APIRouter(dependencies=[Depends(signature_verification)])
 
 
-@router.post("/create_user", response_model=CreatedUser)
+@router.post("/create_user", response_model=CreatedUser, status_code=status.HTTP_201_CREATED)
 def create_user(payload: UserInfo):
     customer_id = str(uuid.uuid4())
 
